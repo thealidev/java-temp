6955ad6ea8dfcc16afa44b6277c8a4f00ef634d8
thealixtech <thealixtech@gmail.com>
2022-06-25 14:55:28 +0500

    .

---
+++ medium/Class-Function-Call.java
@@ -0,0 +1,19 @@
+class print {
+    String a = "Hi ! My Name Is Muhammad Ali. ";
+    String b = "I Am Eighteen Years Old. ";
+    void print(){}
+    void a(){
+        System.out.println(a);
+    }
+    void b(){
+        System.out.println(b);
+    }
+}
+class main{
+    void main(){}
+    public static void main(String[] args) {
+        print abc = new print();
+        abc.a();
+        abc.b();
+    }
+}
